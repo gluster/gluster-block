bf32373936eca15dcdb2af68e8e6787207b441a5
Xiubo Li <xiubli@redhat.com>
2018-11-06 18:05:44 +0800

    gluster-blockd: defend for NULL to avoid crash
    Reviewed-by: Prasanna Kumar Kalever <prasanna.kalever@redhat.com>
    Signed-off-by: Xiubo Li <xiubli@redhat.com>

--- rpc/block_svc_routines.c
+++ rpc/block_svc_routines.c
@@ -3863,6 +3863,9 @@ block_create_cli_1_svc_st(blockCreateCli *blk, struct svc_req *rqstp)
   reply->exit = -1;
 
   list = blockServerParse(blk->block_hosts);
+  if (!list) {
+    goto optfail;
+  }
 
   /* Fail if mpath > list->nhosts */
   if (blk->mpath > list->nhosts) {
@@ -4322,6 +4325,9 @@ block_create_common(blockCreate *blk, char *rbsize, char *prio_path)
   }
 
   list = blockServerParse(blk->block_hosts);
+  if (!list) {
+    goto out;
+  }
 
   /* i = 2; because tpg1 is created by default while iqn create */
   for (i = 2; i <= list->nhosts; i++) {
